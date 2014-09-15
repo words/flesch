48325e559a46ebc527802f33019fb2634b8b0dcd
wooorm <tituswormer@gmail.com>
2014-09-15 11:12:32 +0200

    0.0.1

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * The constants as defined by the revised Flesch Reading Ease Formula.
+ */
+
+var SENTENCE_WEIGHT = 1.015,
+    WORD_WEIGHT = 84.6,
+    BASE = 206.835;
+
+/**
+ * Get the grade level of a given value according to the Flesch Reading Ease
+ * Formula. More information is available at WikiPedia:
+ *
+ *   http://en.wikipedia.org/wiki/
+ *   Flesch–Kincaid_readability_tests#Flesch_Reading_Ease
+ *
+ * @param {Object} counts
+ * @param {number} counts.word - Number of words.
+ * @param {number} counts.sentence - Number of sentences.
+ * @param {number} counts.syllable - Number of syllables.
+ * @return {number}
+ */
+
+function flesch(counts) {
+    if (!counts || !counts.sentence || !counts.word || !counts.syllable) {
+        return NaN;
+    }
+
+    return BASE -
+        SENTENCE_WEIGHT * (counts.word / counts.sentence) -
+        WORD_WEIGHT * (counts.syllable / counts.word);
+}
+
+/**
+ * Export `flesch`.
+ */
+
+module.exports = flesch;
---
+++ spec/flesch.spec.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ * Module dependencies (flesch, assert).
+ */
+
+var flesch = require('..'),
+    assert = require('assert');
+
+/**
+ * Assert, but up to 6 digits.
+ */
+
+function roundAssert(a, b) {
+    assert(Math.round(a * 1000000) === Math.round(b * 1000000));
+}
+
+/**
+ * Unit tests.
+ */
+
+describe('flesch()', function () {
+    it('should be of type `function`', function () {
+        assert(typeof flesch === 'function');
+    });
+
+    it('should work', function () {
+        var result;
+
+        /**
+         * Return NaN when an invalid value is given.
+         */
+
+        result = flesch();
+
+        assert(result !== result);
+
+        /**
+         * The cat sat on the mat
+         *
+         * Sentences: 1
+         * Words: 6
+         * Syllables: 6
+         */
+
+        roundAssert(flesch({
+            'sentence' : 1,
+            'word' : 6,
+            'syllable' : 6
+        }), 116.145);
+
+        /**
+         * This sentence, taken as a reading passage unto itself, is being
+         * used to prove a point.
+         *
+         * Sentences: 1
+         * Words: 16
+         * Syllables: 22
+         */
+
+        roundAssert(flesch({
+            'sentence' : 1,
+            'word' : 16,
+            'syllable' : 22
+        }), 74.27);
+
+        /**
+         * The Australian platypus is seemingly a hybrid of a mammal and
+         * reptilian creature.
+         *
+         * Sentences: 1
+         * Words: 13
+         * Syllables: 26
+         */
+
+        roundAssert(flesch({
+            'sentence' : 1,
+            'word' : 13,
+            'syllable' : 26
+        }), 24.44);
+    });
+});
