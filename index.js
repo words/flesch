91c04815069be12c44808cf3d814cc34e124398f
Titus Wormer <tituswormer@gmail.com>
2017-07-03 23:03:02 +0200

    Refactor code-style

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 'use strict';
 
-/* Expose. */
 module.exports = flesch;
 
-/* Constants. */
 var SENTENCE_WEIGHT = 1.015;
 var WORD_WEIGHT = 84.6;
 var BASE = 206.835;
 
-/**
- * Get the grade level of a given value according to the
- * Flesch Reading Ease Formula.  More information is available
- * at WikiPedia:
- *
- *   http://en.wikipedia.org/wiki/
- *   Flesch–Kincaid_readability_tests#Flesch_Reading_Ease
- *
- * @param {Object} counts
- * @param {number} counts.word - Number of words.
- * @param {number} counts.sentence - Number of sentences.
- * @param {number} counts.syllable - Number of syllables.
- * @return {number}
- */
 function flesch(counts) {
   if (!counts || !counts.sentence || !counts.word || !counts.syllable) {
     return NaN;
--- test.js
+++ test.js
@@ -1,51 +1,32 @@
 'use strict';
 
-/* Dependencies. */
 var test = require('tape');
 var nan = require('is-nan');
 var flesch = require('./');
 
-/* Formula. */
 test('flesch', function (t) {
   t.ok(nan(flesch()), 'NaN when an invalid value is given');
 
-  /*
-   * The cat sat on the mat.
-   *
-   * Sentences: 1
-   * Words: 6
-   * Syllables: 6
-   */
+  /* The cat sat on the mat.
+   * Sentences: 1, words: 6, syllables: 6. */
   t.equal(round(flesch({
     sentence: 1,
     word: 6,
     syllable: 6
   })), 116.145);
 
-  /*
-   * This sentence, taken as a reading passage unto itself, is being
+  /* This sentence, taken as a reading passage unto itself, is being
    * used to prove a point.
-   *
-   * Sentences: 1
-   * Words: 16
-   * Syllables: 22
-   */
-
+   * Sentences: 1, words: 16, syllables: 22. */
   t.equal(round(flesch({
     sentence: 1,
     word: 16,
     syllable: 22
   })), 74.27);
 
-  /*
-   * The Australian platypus is seemingly a hybrid of a mammal and
+  /* The Australian platypus is seemingly a hybrid of a mammal and
    * reptilian creature.
-   *
-   * Sentences: 1
-   * Words: 13
-   * Syllables: 26
-   */
-
+   * Sentences: 1, words: 13, syllables: 26. */
   t.equal(round(flesch({
     sentence: 1,
     word: 13,
