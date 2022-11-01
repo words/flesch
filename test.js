32dedb20ca20245401549b7418a2f2721f36a355
Titus Wormer <tituswormer@gmail.com>
2022-11-01 14:09:40 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {flesch} from './index.js'
 
-test('flesch', function (t) {
-  // @ts-ignore runtime
-  t.ok(Number.isNaN(flesch()), 'NaN when an invalid value is given')
-  t.equal(round(flesch({sentence: 1, word: 6, syllable: 6})), 116.145)
-  t.equal(round(flesch({sentence: 1, word: 16, syllable: 22})), 74.27)
-  t.equal(round(flesch({sentence: 1, word: 13, syllable: 26})), 24.44)
-  t.end()
+test('flesch', function () {
+  // @ts-expect-error runtime
+  assert.ok(Number.isNaN(flesch()), 'NaN when an invalid value is given')
+  assert.equal(round(flesch({sentence: 1, word: 6, syllable: 6})), 116.145)
+  assert.equal(round(flesch({sentence: 1, word: 16, syllable: 22})), 74.27)
+  assert.equal(round(flesch({sentence: 1, word: 13, syllable: 26})), 24.44)
 })
 
 /**
