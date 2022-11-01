07c780072302c1b16acd46fc4309438e2eaa64bf
Titus Wormer <tituswormer@gmail.com>
2022-11-01 14:12:23 +0100

    Add docs to types

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
+/**
+ * @typedef Counts
+ *   Counts from input document.
+ * @property {number} sentence
+ *   Number of sentences.
+ * @property {number} word
+ *   Number of words.
+ * @property {number} syllable
+ *   Number of syllables.
+ */
+
+/**
+ * @typedef {Counts} FleschCounts
+ *   Deprecated: please use the `Counts` type instead.
+ */
+
 const sentenceWeight = 1.015
 const wordWeight = 84.6
 const base = 206.835
 
 /**
- * @typedef {Object.<string, number>} FleschCounts
- * @property {number} sentence
- * @property {number} word
- * @property {number} syllable
- */
-
-/**
- * Given an object containing the number of words (`word`), the number of sentences (`sentence`), and the number of syllables  (`syllable`) in a document, returns the reading ease associated with the document.
+ * Given an object containing the number of words (`word`), the number of
+ * sentences (`sentence`), and the number of syllables  (`syllable`) in a
+ * document, returns the reading ease associated with the document.
  *
- * Returned values are 120 (every sentence consisting of only two one-syllable words), or lower (including negative values).
- *
- * The values have the following semantics:
- *
- * |     Score    | Semantics                                           |
- * | :----------: | :-------------------------------------------------- |
- * | 90.0 – 100.0 | Easily understood by an average 11-year-old student |
- * |  60.0 – 70.0 | Easily understood by 13- to 15-year-old students    |
- * |  0.0 – 30.0  | Best understood by university graduates             |
- *
- * Therefore we can use the following formula to approximate the average age a student would understand a document at, given score `score`:
- *
- * ```js
- * var age = 20 - Math.floor(score / 10)
- * ```
- *
- * @param {FleschCounts} counts
+ * @param {Counts} counts
+ *   Counts from input document.
  * @returns {number}
+ *   Result is 120 (every sentence consisting of only two one-syllable words)
+ *   or lower (including negative values).
+ *
+ *   The values have the following semantics:
+ *
+ *   |     Score    | Semantics                                           |
+ *   | :----------: | :-------------------------------------------------- |
+ *   | 90.0 – 100.0 | Easily understood by an average 11-year-old student |
+ *   |  60.0 – 70.0 | Easily understood by 13- to 15-year-old students    |
+ *   |  0.0 – 30.0  | Best understood by university graduates             |
+ *
+ *   Therefore we can use the following formula to approximate the average age
+ *   a student would understand a document at, given score `score`:
+ *
+ *   ```js
+ *   const age = 20 - Math.floor(score / 10)
+ *   ```
  */
 export function flesch(counts) {
   if (!counts || !counts.sentence || !counts.word || !counts.syllable) {
